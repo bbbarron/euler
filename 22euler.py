53e02b9d8ce4a4a1a235a5f54181d45e8ca5b507
bbbarron <barrybbarron@gmail.com>
2017-12-24 12:57:25 -0800

    Problem 22 changed to eliminate intermediate prints:

--- 22euler.py
+++ 22euler.py
@@ -7,20 +7,12 @@ For example, when the list is sorted into alphabetical order, COLIN, which is wo
 
 What is the total of all the name scores in the file?
 """
+
 import time
 import csv
 import string
 
 start = time.time()
-
-# TODO
-# Read the CSV file and sort the list
-# position = index + 1
-# create dictionary or tuple for alphabet
-# for name in name_list calculate name score
-# multiply name score by position and add to total
-# add name score to the total score
-# print
 
 with open('p022_names.txt', 'r') as f:
     reader = csv.reader(f)
@@ -28,7 +20,7 @@ with open('p022_names.txt', 'r') as f:
     # Why do I get a list of lists here?
 nmslist[0].sort()
 names_list = nmslist[0]
-print(names_list)
+# print(names_list)
 
 alphabet_number_dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
 # print(alphabet_number_dict)
@@ -36,22 +28,8 @@ total = 0
 for name in names_list:
     nmtotal = 0
     for letter in name:
-        nmtotal += alphabet_number_dict[letter]
-        total += (nmtotal * (names_list.index(name)))
-    print("Grand Total is: ", total)
+        nmtotal += alphabet_number_dict[letter]  # This calc is corect
+    total += (nmtotal * (names_list.index(name) + 1))
+print("Grand Total: ", total)
 
 print("Program completed in", time.time() - start, "seconds")
-
-"""
-f = open("p022_names.txt", 'r')
-char_num_dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12, "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17, "R": 18, "S": 19, "T": 20, "U": 21, "V": 22, "W": 23, "X": 24, "Y": 25, "Z": 26}
-names = sorted(f.read().replace('"', '').split(','), key=str)
-sum = 0
-for ind, val in enumerate(names):
-    temp = 0
-    for x in val:
-        temp += char_num_dict[x]
-    sum += temp * (ind + 1)
-
-print (sum)
-"""
